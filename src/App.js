69001fed12340096a4e4821492c6ed2f04479680
Zohaib <54810131+mirzazohaib@users.noreply.github.com>
2022-09-19 17:21:55 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+function app() {
+  return (
+    <div className="App">
+      <Header />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default app;
---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://gagadget.com/media/cache/3b/04/3b04d8421ed6cbc77a6616f3b8973234.jpg"
+          alt=""
+        />
+      </div>
+      <div className="header_middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header_inputCaret" />
+      </div>
+      <div className="header_right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
